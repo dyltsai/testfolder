e516c8237df47d442a31dc6bbd5545150d33f767
Dylan <dtsai1@hwemail.com>
2024-09-11 08:08:08 -0700

    PrintTotalFileSize method

--- MyFileWriter.java
+++ MyFileWriter.java
@@ -68,5 +68,9 @@ public class MyFileWriter {
             e.printStackTrace();
         }
     }
+    private static void printTotalFileSize(String... fileNames) {
+    
+        System.out.println("Total size of all files: ...TBD... bytes");
+    }
 
 }
